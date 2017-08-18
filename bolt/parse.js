100f1aa4f5b190b1c31fc88cca7fb7b127d891f2
Taras Labiak <kissarat@gmail.com>
2017-08-18 13:03:04 +0300

    some parse

--- bolt/parse.js
+++ bolt/parse.js
@@ -5,56 +5,72 @@ function parseRule(rule, g) {
   }
 }
 
-function parse(syntax, g) {
+function parse(syntax, rule, g) {
+  let a
   let c
-  rules: for (const key in syntax) {
-    const rule = syntax[key]
-    const result = [key]
-    for (let i = 1; i < rule.length; i++) {
-      const isLast = i === rule.length - 1
-      if (c = g.at(i)) {
-        switch (rule[0]) {
-          case 'or':
-            if (rule.indexOf(c) > 0) {
-              result.push(c)
+  let r
+  const result = []
+  rama: for (let i = 1; i < rule.length; i++) {
+    const element = rule[i]
+    const isLast = i === rule.length - 1
+    if (a = g.at(i)) {
+      if (a instanceof Array) {
+        for (const name in syntax) {
+          const sub = syntax[name]
+          if (sub instanceof Array) {
+            r = parse(syntax, sub, g.slice(i - 1))
+            if (true === r) {
+              continue rama
             }
-            break
-
-          case 'group':
-            if (c === rule[i]) {
-              result.push(c)
+            else if (r instanceof Array) {
+              a = r
+              break
             }
-            else {
-              continue rules
-            }
-            break
-
-          case 'opt':
-            if (c === rule[i]) {
-              result.push(c)
-            }
-            else if (0 === i){
-              return true
-            }
-            break
-
-          case 'repeat':
-            if (c === rule[i]) {
-              result.push(c)
-            }
-            else if (isLast) {
-              i = 0
-            }
-            else {
-              continue rules
-            }
-            break
+          }
         }
       }
-      else {
-        return false
+      c = a instanceof Array ? a[0] : a
+      switch (rule[0]) {
+        case 'or':
+          if (rule.indexOf(c) > 0) {
+            result.push(a)
+          }
+          break
+
+        case 'group':
+          if (c === element) {
+            result.push(a)
+          }
+          else {
+            return false
+          }
+          break
+
+        case 'opt':
+          if (c === element) {
+            result.push(a)
+          }
+          else if (0 === i) {
+            return true
+          }
+          break
+
+        case 'repeat':
+          if (c === element) {
+            result.push(a)
+          }
+          else if (isLast) {
+            i = 0
+          }
+          else {
+            return false
+          }
+          break
       }
-      return result
+    }
+    else {
+      return false
     }
   }
+  return result
 }
