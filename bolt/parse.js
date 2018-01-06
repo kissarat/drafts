ebf89ebc30685b2cae54217be4b2d4221b878e34
Labiak Taras kissarat@gmail.com <kissarat@gmail.com>
2018-01-06 21:52:20 +0200

    bad version

--- bolt/parse.js
+++ bolt/parse.js
@@ -1,6 +1,7 @@
 class Language {
   constructor(lexemes = Lexemes.Labiak, syntax = mathSyntax) {
     this.lexemes = lexemes
+    this.gas = 60
     syntax = 'string' === typeof syntax ? parseBackusNaur(scan(syntax)) : syntax
     this.syntax = new Map()
     for (const [assign, name, [...rule]] of syntax) {
@@ -19,29 +20,21 @@ class Language {
     let output = []
     g.index = 0
     while (g.can()) {
-      let r
-      // for (const rule of this.syntax) {
-      //   r = this.parseRule(g, ...rule)
-      //   if (r) {
-      //     output.push(r)
-      //     break
-      //   }
-      // }
-      // for (let i = 0; i < 1; i++) {
-      r = this.parseRule(g, ...this.syntax.get('St'))
-      if (r) {
+      const r = this.parseRule(g, ...this.syntax.get('St'))
+      if ((r instanceof Array && r.length > 0) || 'string' === typeof r) {
         output.push(r)
-        // break
       }
-      // }
-      if (!r) {
-        throw new Error('Rule not found')
+      else if (true !== r) {
+        throw new Error('Rule not found ' + JSON.stringify(r))
       }
     }
     return output
   }
 
   parseRule(g, name, ...rule) {
+    if (this.gas-- <= 0) {
+      throw new Error('Out of gas', rule)
+    }
     let c
     let nodes = []
     let isValidRule = true
@@ -49,18 +42,24 @@ class Language {
     loop: for (; rule[i] && (c = g.at(0)); i++) {
       let part = rule[i]
       let node
+      const lexemeName = part instanceof Array ? part[0] : part
+      const isLexeme = this.lexemes.has(lexemeName)
       let isValid = false
-      if ('string' === typeof part) {
-        const token = this.lexemes.get(part)
-        if (token) {
-          isValid = token
-          node = 'string' === typeof token ? part : c
-          g.index++
-        }
-        else {
+      if (isLexeme) {
+        isValid = lexemeName === (c instanceof Array ? part[0] : part)
+        node = c
+        g.index++
+      }
+      else {
+        if ('string' === typeof part) {
           part = this.syntax.get(part)
+        }
+        if (part instanceof Array) {
           node = this.parseRule(g, ...part)
           isValid = node && (true === node || node.length > 0)
+        }
+        else {
+          throw new Error('Rule is not array ' + JSON.stringify({part, i, rule}))
         }
       }
       if (isValid && true !== node) {
